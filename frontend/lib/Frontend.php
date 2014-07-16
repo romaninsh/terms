770ecb0f8a8c61e624ae01e456ce5327f770f6fc
Romans Malinovskis <romans@agiletoolkit.org>
2014-07-16 16:41:55 +0100

    migrate to agile toolkit

---
+++ frontend/lib/Frontend.php
@@ -0,0 +1,20 @@
+<?php
+class Frontend extends ApiFrontend {
+    function init() {
+        parent::init();
+
+        $this->add('Layout_Basic',null,null,['layout/nearly-guru']);
+        $this->add('jUI');
+
+        $dialog = $this->layout->add('View_Popover');
+        $this->on('click','.do-tooltip',$dialog->showJS());
+
+        $pop=$dialog->add('View')->addClass('atk-align-center');
+
+        $pop->add('P')
+            ->set('I believe that we continue to evolve ourselves throughout all our lives constantly
+                improving our skills. I myself am on my path to become IT Guru.');
+        $pop->add('Button')->set(['Dismiss','button'=>'small', 'icon'=>'close'])->js('click',$dialog->js()->dialog('close'));
+    }
+
+}
