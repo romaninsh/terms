2b265855c364dce29b77ddb22bb8e61008a61cae
Romans Malinovskis <romans@agiletoolkit.org>
2014-07-16 16:44:07 +0100

    proper update

--- update.sh
+++ update.sh
@@ -1,66 +1,63 @@
 #!/bin/bash
 
-set -e	 # exit if anything fails
+# Update the files from Git
+git pull
 
-config='../config.php';
-[ -f $config ] || config='../config-default.php';
-[ -f $config ] || config='../../config.php';
-[ -f $config ] || config='../../config-default.php';
-[ -f $config ] || config='../../../config.php';
+# First, get composer there
+if [ -f composer.json ]; then
+    if [ -f composer.phar ]; then
+        echo "== STEP 1 == Updating composer dependencies"
+        php composer.phar self-update
+        php composer.phar update
+    else
+        echo "== STEP 1 == Installing composer dependencies"
+        curl -sS https://getcomposer.org/installer | php
+        php composer.phar install
 
-dsn=`cat $config | grep "config\['dsn'\]" | cut -d= -f2 | sed "s/'/\"/g" | cut -d\" -f2 | sed 's|mysql://||'`
-u=`echo $dsn | cut -d: -f1`
-p=`echo $dsn | cut -d: -f2 | cut -d@ -f1`
-db=`echo $dsn | cut -d/ -f2`
+        # If there are some folders around, link them
+        echo "== STEP 2 == Linking files"
+        CSS=""
+        [ -x ~/Sites/agiletoolkit-css ] && CSS="$HOME/Sites/agiletoolkit-css"
+        [ -x /var/www/agiletoolkit-css ] && CSS="/var/www/agiletoolkit-css"
+        [ -x ~/www/agiletoolkit-css ] && CSS="$HOME/www/agiletoolkit-css"
 
-[ "$db" ] || { echo "Error: Can't read config file"; exit; }
+        if [ "$CSS" ]; then
+            echo "== STEP 2.1 == Use CSS"
+            echo "HEY, I found CSS library in $CSS, so I'm going to use it"
+            ln -fs $CSS .
+        fi
 
-if [ -f ~/.my.cnf ]; then
-	mysql="mysql $db"
-else
-	mysql="mysql -u$u -p$p $db"
+        echo "== STEP 3 == Link ATK"
+        ATK=""
+        [ -x ~/Sites/atk4 ] && ATK="$HOME/Sites/atk4"
+        [ -x ~/Sites/atk43 ] && ATK="$HOME/Sites/atk43"
+        if [ "$ATK" ]; then
+            echo "HEY, I found ATK library in $ATK, so I'm going to link it into vendor/atk4/atk4"
+            rm -rf vendor/atk4/atk4
+            ln -fs $ATK vendor/atk4/atk4
+
+            # Need to make sure composer is not getting upset about it
+        fi
+
+        echo "== STEP 4 == Fixing borked Composer"
+        # composer fucks up few things, so fix them
+        ( cd vendor/atk4/atk4; git remote rm composer )
+        ( cd vendor/atk4/atk4; git checkout 4.3 )
+        ( cd vendor/atk4/atk4; git remote add composer git://github.com/atk4/atk4.git )
+
+        touch config-auto.php
+        chmod 777 config-auto.php
+    fi
 fi
 
-echo "* Applying updates on database '$db'"
-
-cd dbupdates
-for x in *.sql; do 
-	[ -f $x.ok ] && continue
-	echo -n " $x... "
-	
-	# user and password must be in ~/.my.cnf
-	if $mysql -BN < $x 2> $x.fail > $x.out; then
-        cat $x.out | tr -d "\n"
-		mv $x.out $x.ok
-        rm -f $x.fail
-		echo 'ok'
-	else
-        cat $x.out | tr -d "\n"
-        rm -f $x.out
-		echo 'fail'
-		cat $x.fail
-		echo
-        break;
-	fi
+echo "== STEP 5 == Link ATK"
+cat dependencies | while read dir path; do
+  if [ -x $dir ]; then
+    echo " => Updating $dir"
+    ( cd $dir; git pull )
+  else
+    echo " => Installing $dir from $path"
+    mkdir -p $dir
+    ( cd $dir; git clone $path . )
+  fi
 done
-cd ..
-
-if [ -d 'storedfx' ]; then
-	echo "* Re-Importing stored procedures"
-	
-	cnt=0
-	for x in storedfx/*.sql ; do 
-		if $mysql -B < $x 2> $x.fail ; then
-			rm $x.fail
-			cnt=$(( $cnt + 1 ))
-		else
-			echo -n " $x... "
-			echo 'fail'
-			cat $x.fail
-			echo
-		fi
-	done
-	echo " $cnt procedures imported"
-fi
-
-echo "* Done"
