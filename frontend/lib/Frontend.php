674e24cc16e5fb9634f9e9774f686c630bf664bc
Romans Malinovskis <romans@agiletoolkit.org>
2014-07-18 11:04:32 +0100

    grammar fixes

--- frontend/lib/Frontend.php
+++ frontend/lib/Frontend.php
@@ -7,16 +7,6 @@ class Frontend extends ApiFrontend {
         $this->add('jUI');
 
         $this->add('agile55/ga/Controller_Tracker');
-
-        $dialog = $this->layout->add('View_Popover');
-        $this->on('click','.do-tooltip',$dialog->showJS());
-
-        $pop=$dialog->add('View')->addClass('atk-align-center');
-
-        $pop->add('P')
-            ->set('I believe that we continue to evolve ourselves throughout all our lives constantly
-                improving our skills. I myself am on my path to become IT Guru.');
-        $pop->add('Button')->set(['Dismiss','button'=>'small', 'icon'=>'close'])->js('click',$dialog->js()->dialog('close'));
     }
 
 }
